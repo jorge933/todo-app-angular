a990170e9276bb1355965e2402725d9fe0d1b403
RyanDrop <ryangithub7509@gmail.com>
2021-12-27 15:45:03 -0300

    ✨ feat: Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
