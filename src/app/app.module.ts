54cf236c727578247d20003e314c454c494b05ba
Jorge <jorgevitorp9@gmail.com>
2021-12-28 13:06:13 -0300

    📦 refactor: remove imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { FilterComponent } from './components/filter/filter.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,8 +11,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
-  declarations: [AppComponent, TasksComponent, FilterComponent, TaskCardComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatCheckboxModule, FormsModule, MatIconModule, MatButtonModule],
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    FilterComponent,
+    TaskCardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
