e5da5d1629870360d1f0fa9ce903e36a82e5017f
Jorge <jorgevitorp9@gmail.com>
2021-12-28 11:45:23 -0300

    ✨ feat: importing material components and forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 import { FilterComponent } from './components/filter/filter.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, TasksComponent, FilterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatCheckboxModule, FormsModule, MatIconModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
