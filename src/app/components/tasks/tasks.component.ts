c864302065662ef3b3355f59359dfa319435a7c1
Jorge <jorgevitorp9@gmail.com>
2021-12-28 13:12:00 -0300

    📦 refactor: extract the task card to a component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'todo-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
-  checked: boolean = false;
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TasksComponent { }
